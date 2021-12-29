c8f991cfed850d15a0c1736200f1ae751fa62e57
Meshackgit <meshmwangi828@gmail.com>
2021-12-29 16:18:39 +0300

    modified the portfolio Jquery to work effectively

--- js/script.js
+++ js/script.js
@@ -1,35 +1,38 @@
   $(document).ready(function(){
     $("#design-png").on("click",function(){
-      $("#design-png").slideDown().hide('1000');
-      $("#design").show();
+      $("#design-png").hide('1000');
+      $(".design").show();
     });
-    $("#design").on("click",function(){
-      $("#design").slideUp();
-      $("#design-png").slideDown();
+    $(".design").on("click",function(){
+      $(".design").hide();
+      $("#design-png").show('1500');
     });
 
     $("#development-png").on("click",function(){
-      $("#development-png").slideDown().hide('1000');
-      $("#development").show();
+      $("#development-png").hide('1000');
+      $(".development").show();
     });
-    $("#development").on("click",function(){
-      $("#development").slideUp();
-      $("#development-png").slideDown();
+    $(".development").on("click",function(){
+      $(".development").hide();
+      $("#development-png").show('1500');
     });
   
     $("#product-png").on("click",function(){
-      $("#product-png").slideDown().hide('1000');
-      $("#product").show();
+      $("#product-png").hide('1000');
+      $(".product").show();
     });
-    $("#product").on("click",function(){
-      $("#product").slideUp();
-      $("#product-png").slideDown();
+    $(".product").on("click",function(){
+      $(".product").hide();
+      $("#product-png").show('1500');
     });
-  });
+    });
 
-  $("document").ready(function(){
-      $("#development").css("border", "1px solid green");
-      $("#design").css("border", "1px solid green");
-      $("#product").css("border", "1px solid green");
-  });
-  
+    $("document").ready(function(){
+        $(".text").hover(function(){
+            $(this).animate({opacity:'1'});
+            $(".text").show();
+        },
+        function(){
+            $(this).animate({opacity:'1'})
+        });
+    });
