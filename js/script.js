081ac7c1204fe74cb5722ae7e8236c49dafbb8df
Meshackgit <meshmwangi828@gmail.com>
2021-12-29 13:56:22 +0300

    applied Jquery on the what we do section

--- js/script.js
+++ js/script.js
@@ -1,102 +1,35 @@
-$(document).ready(function(){
-    $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
-    });
-    $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
-    });
-  });
-  
   $(document).ready(function(){
-    $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
-      $("#design").show('1500');
+    $("#design-png").on("click",function(){
+      $("#design-png").slideDown().hide('1000');
+      $("#design").show();
     });
-    $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#design-image").slideDown('1500');
+    $("#design").on("click",function(){
+      $("#design").slideUp();
+      $("#design-png").slideDown();
     });
-  });
-  
-  $(document).ready(function(){
-    $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
+
+    $("#development-png").on("click",function(){
+      $("#development-png").slideDown().hide('1000');
+      $("#development").show();
     });
-    $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
-    });
-  });
-  
-  $(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
-    });
-  });
-  
-  $(document).ready(function(){
-    $("#work5").mouseover(function(){
-      $("#overlay5").show();
-    }).mouseout(function(){
-      $("#overlay5").hide();
-    });
-    $("#work6").mouseover(function(){
-      $("#overlay6").show();
-    }).mouseout(function(){
-      $("#overlay6").hide();
-    });
-    $("#work7").mouseover(function(){
-      $("#overlay7").show();
-    }).mouseout(function(){
-      $("#overlay7").hide();
-    });
-    $("#work8").mouseover(function(){
-      $("#overlay8").show();
-    }).mouseout(function(){
-      $("#overlay8").hide();
-    });
-  });
-  
-  $(document).ready(function(){
-    $("form#form34A").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#MERGE1").val();
-      var email = $("input#MERGE0").val();
-      var message = $("textarea#comment").val();
-      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
-      
+    $("#development").on("click",function(){
+      $("#development").slideUp();
+      $("#development-png").slideDown();
     });
   
+    $("#product-png").on("click",function(){
+      $("#product-png").slideDown().hide('1000');
+      $("#product").show();
+    });
+    $("#product").on("click",function(){
+      $("#product").slideUp();
+      $("#product-png").slideDown();
+    });
+  });
+
+  $("document").ready(function(){
+      $("#development").css("border", "1px solid green");
+      $("#design").css("border", "1px solid green");
+      $("#product").css("border", "1px solid green");
   });
   
